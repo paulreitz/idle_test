b696800337d6f104740ef6279e17e88ad3d391b0
paulreitz <avenose@gmail.com>
2025-06-18 14:36:01 -0700

    Added deployment scripts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+const getBase = () => {
+    if (process.env.NODE_ENV === 'development') return '/';
+    return `/${
+    process.env.GITHUB_REPOSITORY?.split('/')[1] || 'idle_test'
+  }/`;
+}
+
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
+  base: getBase(),
   build: {
     target: 'esnext',
   },
